0722e2dbfedb77e62e04ff564beebeddc105c254
ridwanullah <wetwan080@gmail.com>
2025-03-17 18:53:24 +0100

    about page header pro

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,12 @@
+import { assests } from "../assets";
+
 const About = () => {
-  return <div className="h-screen py-16 bg-green-200">About</div>;
+  return (
+    <div
+      className="h-screen py-16 bg-green-200"
+      style={{ backgroundImage: `url(${assests.image})` }}
+    ></div>
+  );
 };
 
 export default About;
