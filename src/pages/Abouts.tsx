3ad2d966491cb1a727c2ac26a17f4199948ceb0f
ridwanullah <wetwan080@gmail.com>
2025-03-21 06:43:14 +0100

    responsiceness for abour, contact page created

--- src/pages/Abouts.tsx
+++ src/pages/Abouts.tsx
@@ -26,7 +26,7 @@ const Abouts = () => {
     <div className="flex items-center justify-center pb-10">
       <Pagebanner title="About us" image={assests.image3} text="about" />
       <About className="w-full  h-screen top-[70vh] absolute text-center -z-50" />
-      <div className=" top-[170vh] h-[45vh] absolute lg:w-4/6 sm:w-5/6 mx-auto ">
+      <div className=" top-[210vh] h-[45vh] absolute lg:w-4/6 sm:w-5/6 mx-auto sm:top-[170vh]">
         <video
           src={assests.vid}
           className="w-full h-full object-cover"
@@ -46,7 +46,7 @@ const Abouts = () => {
           )}
         </div>
       </div>
-      <div className="top-[225vh] h-[50vh] flex items-center justify-center absolute w-full  ">
+      <div className="top-[260vh] h-[50vh] flex items-center justify-center absolute w-full  sm:top-[220vh]">
         <div className=" lg:w-4/6 sm:w-5/6 mx-auto overflow-x-scroll h-5/6 flex items-center justify-center gap-4">
           {images.map((imgs, i) => (
             <img
@@ -63,10 +63,10 @@ const Abouts = () => {
         </div>
       </div>
       {image && (
-        <div className="fixed overflow-hidden z-40 flex items-center justify-center shadow-2xl top-0 bottom-[20vh] right-0 left-0 bg-transparen bg-white/70 p-10 h-full  w-full ">
+        <div className="fixed overflow-hidden z-40 flex items-center justify-center shadow-2xl top-0 bottom-[20vh] right-0 left-0 bg-transparen bg-white p-10 h-full  w-full ">
           <img src={img} alt="" className=" w-[500px]  h-[500px]  " />
           <div
-            className="absolute text-white top-[22%] text-2xl rounded-full p-1 w-10 text-center h-10 left-[62%]  bg-red-600 "
+            className="absolute text-white top-[18%] sm:top-[25%] sm:right-[20%] text-2xl rounded-full p-1 w-10 text-center h-10 right-[7%]  bg-red-600 lg:top-[17%] lg:right-[29%] cursor-pointer"
             onClick={() => SetImage(false)}
           >
             {" "}
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,77 @@
-import React from "react";
 import Pagebanner from "../components/Pagebanner";
 import { assests } from "../assets";
+import Button from "../components/Button";
 
 const Contact = () => {
+  const socials = ["facebook", "instagram", "twitter", "linkedIn"];
   return (
-    <div>
+    <div className="flex items-center justify-center">
       <Pagebanner title="contact us" image={assests.image10} text="contact" />
+      <div className="shadow-2xl h-screen absolute top-[70vh] sm:w-5/6 lg:w-4/6 mx-auto items-center justify-center flex py-10 ">
+        <div className=" w-full lg:w-1/2  h-full p-2">
+          <h2 className="font-bold text-2xl capitalize py-2"> contact us</h2>
+          <p className="text-gray-400 py-2 ">
+            We're open for any suggestion or just to have a chat
+          </p>
+          <div className="grid sm:grid-cols-3 gap-3">
+            <div className="">
+              <span className="font-bold uppercase text-lg text-gray-700 ">
+                Address:
+              </span>
+              <p className="text-gray-400 text-sm">
+                198 West 21th Street, Suite 721 New York NY 10016
+              </p>
+            </div>
+            <div className="">
+              <span className="font-bold uppercase text-lg text-gray-700 ">
+                email:
+              </span>
+              <p className="text-gray-400 text-sm">info@oakberry.com</p>
+            </div>
+            <div className="">
+              <span className="font-bold uppercase text-lg text-gray-700 ">
+                phone:
+              </span>
+              <p className="text-gray-400 text-sm">0987654321</p>
+            </div>
+          </div>
+          <form action=" " className="mt-5 p-3 flex flex-col">
+            <input
+              className="border-b py-4 border-gray-500 placeholder-shown placeholder:capitalize outline-none focus:border-green-500 placeholder:text-gray-600"
+              type="text"
+              placeholder="name"
+            />
+            <input
+              className="border-b py-4 border-gray-500 placeholder-shown placeholder:capitalize outline-none focus:border-green-500 placeholder:text-gray-600"
+              type="email"
+              placeholder="email"
+            />
+            <textarea
+              className="border-b py-4 border-gray-500 placeholder-shown placeholder:capitalize outline-none focus:border-green-500 placeholder:text-gray-600 max-h-36 h-3max-h-36"
+              placeholder="your message"
+            ></textarea>
+            <div className="mt-5 px-1">
+              <Button
+                className="px-12 border bg-green-500 text-white py-6 uppercase font-semibold text-lg hover:bg-white hover:text-green-500 transform-fill ease-in duration-500 cursor-pointer"
+                text="send a message"
+              />
+            </div>
+          </form>
+
+          <div className="flex gap-3 px-4 uppercase mt-4 ">
+            {socials.map((item, i) => (
+              <div
+                className="hover:text-gray-500 cursor-pointer"
+                onClick={() => scrollTo(0, 0)}
+                key={i}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 h-full hidden lg:block p-2"></div>
+      </div>
     </div>
   );
 };
